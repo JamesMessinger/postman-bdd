9727c505202f6eb462412cc81067c81e9afed7ab
James Messinger <james.messinger@gmail.com>
2017-02-05 13:00:11 -0600

    added tests to ensure that all hooks run in the correct order

--- test/specs/hooks.spec.js
+++ test/specs/hooks.spec.js
@@ -1,4 +1,276 @@
-// TODO: Add a test to make sure that ALL hooks run in the correct order
-// TODO: Add a test to make sure that ALL hooks run in the correct order, even if there are failed assertions
-// TODO: Add a test to make sure that ALL hooks run in the correct order, even if an error occurs
-// TODO: Add a test to make sure that ALL hooks run in the correct order, even for nested tests
+'use strict';
+
+const test = require('tape');
+const Postman = require('../fixtures/postman');
+
+test('Hooks run in the correct order', (t) => {
+  let postman = new Postman(t);
+  let i = 0, testNumber = 0;
+
+  before(() => {
+    assert.equal(++i, 1);
+  });
+  before(() => {
+    assert.equal(++i, 2);
+  });
+
+  beforeEach(() => {
+    assert.equal(++i, (testNumber * 5) + 3);
+  });
+  beforeEach(() => {
+    assert.equal(++i, (testNumber * 5) + 4);
+  });
+
+  afterEach(() => {
+    assert.equal(++i, (testNumber * 5) + 1);
+  });
+  afterEach('my third hook', () => {
+    assert.equal(++i, (testNumber * 5) + 2);
+  });
+
+  after(() => {
+    assert.equal(++i, 28);
+  });
+  after(() => {
+    assert.equal(++i, 29);
+  });
+
+  describe(() => {
+    it(() => {
+      testNumber++;
+      assert.equal(++i, 5);
+    });
+
+    describe(() => {
+      it(() => {
+        testNumber++;
+        assert.equal(++i, 10);
+      });
+
+      describe(() => {
+        it(() => {
+          testNumber++;
+          assert.equal(++i, 15);
+        });
+      });
+
+      it(() => {
+        testNumber++;
+        assert.equal(++i, 20);
+      });
+    });
+
+    it(() => {
+      testNumber++;
+      assert.equal(++i, 25);
+    });
+  });
+
+  t.equal(i, 29);
+
+  postman.checkTests({
+    'describe #1 test #1': true,
+    'describe #1 describe #2 test #2': true,
+    'describe #1 describe #2 describe #3 test #3': true,
+    'describe #1 describe #2 test #4': true,
+    'describe #1 test #5': true,
+  });
+
+  t.end();
+});
+
+test('Hooks run in the correct order even if there are failed assertions', (t) => {
+  let postman = new Postman(t);
+  let i = 0, testNumber = 0;
+
+  before(() => {
+    assert.equal(++i, 9999);
+  });
+  before(() => {
+    assert.equal(++i, 2);
+  });
+
+  beforeEach(() => {
+    assert.equal(++i, 9999);
+  });
+  beforeEach(() => {
+    assert.equal(++i, (testNumber * 5) + 4);
+  });
+
+  afterEach(() => {
+    assert.equal(++i, 9999);
+  });
+  afterEach('my third hook', () => {
+    assert.equal(++i, (testNumber * 5) + 2);
+  });
+
+  after(() => {
+    assert.equal(++i, 9999);
+  });
+  after(() => {
+    assert.equal(++i, 29);
+  });
+
+  describe(() => {
+    it(() => {
+      testNumber++;
+      assert.equal(++i, 5);
+    });
+
+    describe(() => {
+      it(() => {
+        testNumber++;
+        assert.equal(++i, 10);
+      });
+
+      describe(() => {
+        it(() => {
+          testNumber++;
+          assert.equal(++i, 15);
+        });
+      });
+
+      it(() => {
+        testNumber++;
+        assert.equal(++i, 20);
+      });
+    });
+
+    it(() => {
+      testNumber++;
+      assert.equal(++i, 25);
+    });
+  });
+
+  t.equal(i, 29);
+
+  postman.checkTests({
+    'before #1': false,
+    'expected 1 to equal 9999': false,
+    'describe #1 beforeEach #1': false,
+    'expected 3 to equal 9999': false,
+    'describe #1 test #1': true,
+    'describe #1 afterEach #1': false,
+    'expected 6 to equal 9999': false,
+
+    'describe #1 describe #2 beforeEach #1': false,
+    'expected 8 to equal 9999': false,
+    'describe #1 describe #2 test #2': true,
+    'describe #1 describe #2 afterEach #1': false,
+    'expected 11 to equal 9999': false,
+
+    'describe #1 describe #2 describe #3 beforeEach #1': false,
+    'expected 13 to equal 9999': false,
+    'describe #1 describe #2 describe #3 test #3': true,
+    'describe #1 describe #2 describe #3 afterEach #1': false,
+    'expected 16 to equal 9999': false,
+
+    'expected 18 to equal 9999': false,
+    'describe #1 describe #2 test #4': true,
+    'expected 21 to equal 9999': false,
+
+    'expected 23 to equal 9999': false,
+    'describe #1 test #5': true,
+    'expected 26 to equal 9999': false,
+    'after #1': false,
+    'expected 28 to equal 9999': false,
+  });
+
+  t.end();
+});
+
+test('Hooks run in the correct order even if an error occurs', (t) => {
+  let postman = new Postman(t);
+  let i = 0, testNumber = 0;
+
+  before(() => {
+    assert.equal(++i, 1);
+    throw new Error('error in before hook');
+  });
+  before(() => {
+    assert.equal(++i, 2);
+  });
+
+  beforeEach(() => {
+    assert.equal(++i, (testNumber * 5) + 3);
+    throw new Error('error in beforeEach hook');
+  });
+  beforeEach(() => {
+    assert.equal(++i, (testNumber * 5) + 4);
+  });
+
+  afterEach(() => {
+    assert.equal(++i, (testNumber * 5) + 1);
+    throw new Error('error in afterEach hook');
+  });
+  afterEach('my third hook', () => {
+    assert.equal(++i, (testNumber * 5) + 2);
+  });
+
+  after(() => {
+    assert.equal(++i, 28);
+    throw new Error('error in after hook');
+  });
+  after(() => {
+    assert.equal(++i, 29);
+  });
+
+  describe(() => {
+    it(() => {
+      testNumber++;
+      assert.equal(++i, 5);
+    });
+
+    describe(() => {
+      it(() => {
+        testNumber++;
+        assert.equal(++i, 10);
+      });
+
+      describe(() => {
+        it(() => {
+          testNumber++;
+          assert.equal(++i, 15);
+        });
+      });
+
+      it(() => {
+        testNumber++;
+        assert.equal(++i, 20);
+      });
+    });
+
+    it(() => {
+      testNumber++;
+      assert.equal(++i, 25);
+    });
+  });
+
+  t.equal(i, 29);
+
+  postman.checkTests({
+    'before #1': false,
+    'error in before hook': false,
+    'describe #1 beforeEach #1': false,
+    'error in beforeEach hook': false,
+    'describe #1 test #1': true,
+    'describe #1 afterEach #1': false,
+    'error in afterEach hook': false,
+
+    'describe #1 describe #2 beforeEach #1': false,
+    'describe #1 describe #2 test #2': true,
+    'describe #1 describe #2 afterEach #1': false,
+
+    'describe #1 describe #2 describe #3 beforeEach #1': false,
+    'describe #1 describe #2 describe #3 test #3': true,
+    'describe #1 describe #2 describe #3 afterEach #1': false,
+
+    'describe #1 describe #2 test #4': true,
+
+    'describe #1 test #5': true,
+    'after #1': false,
+    'error in after hook': false,
+  });
+
+  t.end();
+});
