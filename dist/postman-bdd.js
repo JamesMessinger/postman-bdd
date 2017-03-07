d95a9545f9afc4cd7ab20e0d68b51ac28457b29d
James Messinger <james.messinger@gmail.com>
2017-03-07 10:41:28 -0600

    release v4.1.0

--- dist/postman-bdd.js
+++ dist/postman-bdd.js
@@ -1,5 +1,5 @@
 /*!
- * Postman BDD v4.0.1 (March 6th 2017)
+ * Postman BDD v4.1.0 (March 7th 2017)
  * 
  * https://bigstickcarpet.github.io/postman-bdd
  * 
@@ -24,10 +24,11 @@ module.exports = chaiHttp;
 
 /**
  * This module is based on chai-http
- * https://github.com/chaijs/chai-http
  *
  * Copyright(c) 2011-2012 Jake Luer <jake@alogicalparadox.com>
  * MIT Licensed
+ *
+ * @see {@link https://github.com/chaijs/chai-http}
  */
 function chaiHttp (chai, _) {
   var Assertion = chai.Assertion;
@@ -394,7 +395,7 @@ function chaiHttp (chai, _) {
  * @returns {String|Undefined}
  */
 function getHeader (obj, key) {
-  // SuperAgent `response` object
+  // superAgent.Response object
   if (obj.getHeader) {
     return obj.getHeader(key);
   }
@@ -416,7 +417,7 @@ function getHeader (obj, key) {
  * @returns {object[]}
  */
 function getCookies (obj) {
-  // SuperAgent `response` object
+  // superAgent.Response` object
   if (obj.cookies) {
     return obj.cookies;
   }
@@ -434,7 +435,7 @@ function getCookies (obj) {
  * @returns {String|Undefined}
  */
 function getCookie (obj, name) {
-  // SuperAgent `response` object
+  // superAgent.Response object
   if (obj.getCookie) {
     return obj.getCookie(name);
   }
@@ -708,7 +709,7 @@ Hook.prototype.push = function (title, fn) {
 /* eslint strict:off */
 
 var PostmanBDD = require('./bdd');
-var SuperAgent = require('./response');
+var superAgent = require('./response');
 var assertions = require('./assertions');
 var options = require('./options');
 var log = require('./log');
@@ -749,8 +750,7 @@ function initBDD () {
  * Defines (or redefines) SuperAgent globals
  */
 function initSuperAgent () {
-  var superAgent = new SuperAgent();
-  response = superAgent.response;
+  response = new superAgent.Response();
 }
 
 /**
@@ -833,321 +833,148 @@ module.exports = {
 var log = require('./log');
 var cookies = require('./cookies');
 
+module.exports = {
+  /**
+   * SuperAgent's Response API
+   *
+   * @see {@link https://visionmedia.github.io/superagent/#response-properties}
+   */
+  Response: Response
+};
+
+function Response () {
+  /**
+   * The HTTP response status code
+   * @type {number}
+   */
+  this.status = typeof responseCode === 'object' ? responseCode.code : 0;
+
+  /**
+   * The HTTP response status type (1, 2, 3, 4, or 5)
+   * @type {number}
+   */
+  this.statusType = Math.floor(this.status / 100);
+
+  /**
+   * Named properties for common HTTP status codes
+   *
+   * @see {@link https://visionmedia.github.io/superagent/#response-status}
+   * @type {boolean}
+   */
+  this.info = this.statusType === 1;
+  this.ok = this.statusType === 2;
+  this.redirect = this.statusType === 3;
+  this.clientError = this.statusType === 4;
+  this.serverError = this.statusType === 5;
+  this.error = this.clientError || this.serverError;
+  this.accepted = this.status === 202;
+  this.noContent = this.status === 204 || this.status === 1223;
+  this.badRequest = this.status === 400;
+  this.unauthorized = this.status === 401;
+  this.notAcceptable = this.status === 406;
+  this.notFound = this.status === 404;
+  this.forbidden = this.status === 403;
+
+  /**
+   * The response time, in milliseconds
+   * @type {number}
+   */
+  this.time = typeof responseTime === 'number' ? responseTime : 0;
+
+  /**
+   * The parsed response headers, with lowercased field names.
+   *
+   * NOTE: SuperAgent exposes this property as both `header` and `headers`
+   *
+   * @type {object}
+   */
+  this.headers = this.header = pojo(responseHeaders);
+
+  var contentType = this.getHeader('content-type') || '';
+  var charsetMatch = /charset=([a-zA-Z0-9_-]+)/i.exec(contentType);
+
+  /**
+   * The value of the Content-Type header without the charset (e.g. "text/html")
+   * @type {string}
+   */
+  this.type = contentType.split(';')[0];
+
+  /**
+   * The value of the Content-Type header without the MIME type (e.g. "utf8")
+   * @type {string}
+   */
+  this.charset = charsetMatch ? charsetMatch[1] : '';
+
+  /**
+   * The parsed response cookies.
+   * @type {object[]}
+   */
+  this.cookies = parseCookies(this);
+
+  /**
+   * Returns the unparsed response body string
+   * @type {string}
+   */
+  this.text = typeof responseBody === 'string' ? responseBody : '';
+
+  /**
+   * The parsed response body.
+   * @type {object}
+   */
+  this.body = parseResponeBody(this);
+}
+
 /**
- * SuperAgent's Response API
+ * Returns the value of the given header.  Header names are case insensitive.
  *
- * For more info, see:
- * https://visionmedia.github.io/superagent/#response-properties
+ * @see {@link https://visionmedia.github.io/superagent/#response-content-type}
+ * @param {string} name
+ * @returns {?string}
  */
-module.exports = function SuperAgent () {
-  var superAgent = this;
-
-  superAgent.response = {
-    /**
-     * Returns the value of the given header.  Header names are case insensitive.
-     *
-     * https://visionmedia.github.io/superagent/#response-content-type
-     *
-     * @param {string} name
-     * @returns {?string}
-     */
-    getHeader: function (name) {
-      if (typeof postman === 'object' && typeof postman.getResponseHeader === 'function') {
-        return postman.getResponseHeader(name);
-      }
-      else {
-        name = (name || '').toLowerCase();
-        return this.headers[name];
-      }
-    },
-
-    /**
-     * Returns the value of the given cookie.
-     *
-     * @param {string} name
-     * @returns {?string}
-     */
-    getCookie: function (name) {
-      if (typeof postman === 'object' && typeof postman.getResponseCookie === 'function') {
-        return postman.getResponseCookie(name);
-      }
-      else {
-        return cookies.getCookie(this.cookies, name);
-      }
-    },
-  };
-
-  defineProperties(superAgent.response, {
-    /**
-     * Returns the HTTP response status code
-     * @type {number}
-     */
-    status: function () {
-      return typeof responseCode === 'object' ? responseCode.code : 0;
-    },
-
-    /**
-     * Returns the HTTP response status type (1, 2, 3, 4, or 5)
-     * @type {number}
-     */
-    statusType: function () {
-      return Math.floor(this.status / 100);
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "info" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    info: function () {
-      return this.statusType === 1;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "ok" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    ok: function () {
-      return this.statusType === 2;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "redirect" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    redirect: function () {
-      return this.statusType === 3;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "client error" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    clientError: function () {
-      return this.statusType === 4;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "server error" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    serverError: function () {
-      return this.statusType === 5;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP error status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    error: function () {
-      return this.clientError || this.serverError;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "accepted" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    accepted: function () {
-      return this.status === 202;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "no content" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    noContent: function () {
-      return this.status === 204 || this.status === 1223;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "bad request" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    badRequest: function () {
-      return this.status === 400;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "unauthorized" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    unauthorized: function () {
-      return this.status === 401;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "not acceptable" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    notAcceptable: function () {
-      return this.status === 406;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "not found" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    notFound: function () {
-      return this.status === 404;
-    },
-
-    /**
-     * Indicates whether the response is an HTTP "forbidden" status
-     *
-     * https://visionmedia.github.io/superagent/#response-status
-     *
-     * @type {boolean}
-     */
-    forbidden: function () {
-      return this.status === 403;
-    },
-
-    /**
-     * Returns the unparsed response body string
-     * @type {string}
-     */
-    text: function () {
-      return typeof responseBody === 'string' ? responseBody : '';
-    },
-
-    /**
-     * The parsed response body.
-     * @type {object}
-     */
-    body: function () {
-      var parsedBody = parseResponeBody(this);
-
-      // Replace this getter function with the parsed response body
-      Object.defineProperty(this, 'body', {
-        configurable: true,
-        enumerable: true,
-        writable: true,
-        value: parsedBody,
-      });
-
-      return parsedBody;
-    },
-
-    /**
-     * The parsed response headers, with lowercased field names.
-     *
-     * NOTE: SuperAgent exposes this property as both `header` and `headers`
-     *
-     * @type {object}
-     */
-    header: function () {
-      return typeof responseHeaders === 'object' ? responseHeaders : {};
-    },
-
-    /**
-     * The parsed response headers, with lowercased field names.
-     *
-     * NOTE: SuperAgent exposes this property as both `header` and `headers`
-     *
-     * @type {object}
-     */
-    headers: function () {
-      return typeof responseHeaders === 'object' ? responseHeaders : {};
-    },
-
-    /**
-     * The response time, in milliseconds
-     * @type {number}
-     */
-    time: function () {
-      return typeof responseTime === 'number' ? responseTime : 0;
-    },
-
-    /**
-     * Returns the value of the Content-Type header without the charset (e.g. "text/html")
-     * @type {string}
-     */
-    type: function () {
-      var contentType = this.getHeader('content-type') || '';
-      return contentType.split(';')[0];
-    },
-
-    /**
-     * Returns the value of the Content-Type header without the MIME type (e.g. "utf8")
-     * @type {string}
-     */
-    charset: function () {
-      var contentType = this.getHeader('content-type') || '';
-      var match = /charset=([a-zA-Z0-9_-]+)/i.exec(contentType);
-      if (!match) { return ''; }
-      return match[1];
-    },
-
-    /**
-     * The parsed response cookies.
-     *
-     * @type {object[]}
-     */
-    cookies: function () {
-      if (typeof responseCookies === 'object' && responseCookies.length > 0) {
-        return responseCookies;
-      }
-
-      var header = this.getHeader('set-cookie');
-      return cookies.parseResponseCookies(header);
-    },
-  });
+Response.prototype.getHeader = function (name) {
+  if (typeof postman === 'object' && typeof postman.getResponseHeader === 'function') {
+    return postman.getResponseHeader(name);
+  }
+  else {
+    name = (name || '').toLowerCase();
+    return this.headers[name];
+  }
 };
 
 /**
- * Defines the read-only property getters on the given object
+ * Returns the value of the given cookie.
  *
- * @param {object} getters
+ * @param {string} name
+ * @returns {?string}
  */
-function defineProperties (obj, getters) {
-  Object.keys(getters).forEach(function (name) {
-    Object.defineProperty(obj, name, {
-      configurable: true,
-      enumerable: true,
-      get: getters[name],
-    });
-  });
+Response.prototype.getCookie = function (name) {
+  if (typeof postman === 'object' && typeof postman.getResponseCookie === 'function') {
+    return postman.getResponseCookie(name);
+  }
+  else {
+    return cookies.getCookie(this.cookies, name);
+  }
+};
+
+/**
+ * Returns the parsed "Set-Cookie" headers
+ *
+ * @param {Response} response
+ * @returns {object[]}
+ */
+function parseCookies (response) {
+  if (typeof responseCookies === 'object' && responseCookies.length > 0) {
+    return responseCookies;
+  }
+
+  var header = response.getHeader('set-cookie');
+  return cookies.parseResponseCookies(header);
 }
 
 /**
  * Parses the response body as JSON, XML, or plain-text
  *
- * @param {object} response
+ * @param {Response} response
  * @returns {object|string}
  */
 function parseResponeBody (response) {
@@ -1164,12 +991,7 @@ function parseResponeBody (response) {
   if (response.type.indexOf('xml') >= 0) {
     // The response looks like an XML mime type (e.g. "text/xml", "application/soap+xml", etc.)
     try {
-      var json = xml2Json(response.text);
-
-      // NOTE: The objects that xml2Json returns don't inherit from the Object prototype,
-      // which means they don't work with Chai's `should` syntax.  So we re-parse the JSON
-      // structure to convert the objects to "normal" objects that work with Chai.
-      return JSON.parse(JSON.stringify(json));
+      return pojo(xml2Json(response.text));
     }
     catch (err) {
       log.error('Unable to parse the response body as XML', log.errorToPOJO(err));
@@ -1178,6 +1000,18 @@ function parseResponeBody (response) {
 
   // If all else fails, just return the response body as plain-text
   return response.text;
+}
+
+/**
+ * Converts the given object to a POJO (plain-old JavaScript object).
+ * This is necessary because many Postman objects don't inherit from the Object prototype,
+ * which means that they can't be used with Chai.js `should` interface.
+ *
+ * @param {object} [obj]
+ * @returns {object}
+ */
+function pojo (obj) {
+  return JSON.parse(JSON.stringify(obj || {}));
 }
 
 },{"./cookies":3,"./log":6}],9:[function(require,module,exports){
